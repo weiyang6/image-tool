b512ec44fd207f57b3b0049d23239773568ae229
weiyang6 <weiyang_dev@163.com>
2020-03-18 00:09:02 +0800

    测试git

---
+++ src/main/java/com/iflytek/qiming/jkpt/util/StringUtil.java
@@ -0,0 +1,11 @@
+package com.iflytek.qiming.jkpt.util;
+
+/**
+ * @ClassName StringUtil
+ * @Description TODO
+ * @Author weiyang
+ * @Date 2020/3/18 0:08
+ * @Version 1.0
+ **/
+public class StringUtil {
+}
